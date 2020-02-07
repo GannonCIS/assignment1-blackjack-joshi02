3c299352378fb671411886160dba3ba66117c892
Keerthi Katharaki Hatti <gandham003@gannon.edu>
2020-02-06 23:57:06 -0500

    Complete The Deck Structure

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -10,27 +10,48 @@ package blackjack;
  * @author gubotdev
  */
 public class Deck {
-    private Card[] mycards = new Card[52];
-    private String[] ranks = {};
-    private String[] suits = {};
+    private Card[] myCards = new Card[52];
+    private String[] ranks = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", 
+        "10", "Jack", "Queen", "King"};
+    private String[] suits = {"CLubs", "Diamonds", "Hearts", "Spades"};
     private int nextCard = 0;
     
-    
-    
-    
-    
-    private void intiDeck(){
+    public Deck(){
+        initDeck();
+        shuffle();
+    }
+    private void initDeck(){
+        int index = 0;
+        for(int s = 0; s < suits.length; s++){
+            for(int r = 0; r < ranks.length; r++){
+                myCards[index] = new Card(ranks[r], suits[s]);
+                index++;
+            }
+        }
         
     }
     private void shuffle(){
-        
+        for(int i = 0; i < myCards.length; i++){
+            Card temp = myCards[i];
+            int rand = (int)Math.random()*52;
+            myCards[i] = myCards[rand];
+            myCards[rand] = temp;
+        }
     }
     //printDeck is for testing only --delete me
     public void printDeck(){
+        for(int i=0; i < myCards.length; i++ ){
+            System.out.println(myCards[i].RANK + " of " + myCards[i].SUIT);
+        }
         
     }
     public Card dealCard(){
-        return null; //donot forget to change this!!
+        Card next = myCards[nextCard];
+        nextCard++;
+        if(nextCard > 51){
+            shuffle();
+            nextCard = 0;
+        }
+        return next; 
     }
 }
-
