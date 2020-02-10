74deadf1f5c79179953a08ddac5a3aac16ce1936
Keerthi Katharaki Hatti <gandham003@gannon.edu>
2020-02-10 11:49:10 -0500

    Complete The Hand Structure

--- src/blackjack/Dealer.java
+++ src/blackjack/Dealer.java
@@ -5,30 +5,63 @@
  */
 package blackjack;
 
+import java.util.Scanner;
 /**
  *
  * @author gubotdev
  */
 public class Dealer {
-    private int handValue = 0;
-    
-    private int winner = 0;
-    
+    private Player[] myPlayers;
+    private Deck myDeck = new Deck();
     private Hand dealerHand = new Hand();
+    private Scanner scan = new Scanner(System.in);
     
-    public Card dealOpeningHands(){
-        return null;
+    public Dealer(int numOfPlayers){
+        
+        initMyPlayers(numOfPlayers);
     }
-    public int takePlayerTurns(Deck deck){
-        return handValue;
+    public void dealOutOpeningHand(){
+        for(int i = 0; i<2; i++){
+            for(Player currPlayer : myPlanner){
+                currPlayer.getMyHand().addCard(myDeck.dealCard());
+            }
+            dealerHand.addCard(myDeck.dealCard());
+        }
+    }
+    public void takePlayerTurn(){
+        for(Player currPlayer : myPlayers){
+                while(currPlayer.getMyHand().getNumOfCards() < 5 &&
+                        currPlayer.getMyHand().getScore() < 21){
+                    System.out.println(currPlayer.getName + "s Hand");
+                    currPlayer.getMyHand().printHand();
+                    System.out.println("Wanna Hit? (y/n)");
+                    char opt = scan.next().charAt(0);
+                    if(opt == "y"){
+                        currPlayer.getMyHand().addCard(myDeck.dealCard());
+                }
+            }
+            currPlayer.getMyHand().printHand();
+        }
+    }
+    public void playerOutDealerHand(){
+        while(dealerHand.getScore() < 16){
+            dealerHand.addCard(myDeck.dealCard());
+        }
+        dealerHand.printHand();
+    }
+    public void declareWinners(){
         
     }
-    
-    public void playerOutDealerHand() {
-        
-    }
-    
-    public int declareWinner(){
-        return winner;
+    private void initMyPlayers(int numPlayers){
+        myPlayers = new Player(numPlayers);
+        for(int i = 0; i < myPlayers.length; i++){
+            System.out.print("Player"+ (i+1) + "What is your name");
+            String name = scan.next();
+            if(name.equals("")){
+                myPlayers[i] = new Player(i+1);
+            }else{
+                myPlayers[i] = new Player(name);
+            }
+        }
     }
 }
